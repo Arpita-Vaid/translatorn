847172da3adf4154dbb90396aa1d4bdc6e9387ff
Arpita-vaid <arpitavaid6@gmail.com>
2026-05-11 12:39:37 +0530

    fixed render port

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def index():
     return render_template('index.html', translated_text=translated_text)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host='0.0.0.0', port=10000)
