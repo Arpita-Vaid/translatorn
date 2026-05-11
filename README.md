c03fb9bc2fc9d54d40d3aab65932ed5f77edc014
Arpita-vaid <arpitavaid6@gmail.com>
2026-05-11 12:04:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# English to Hindi Translator
+
+This is a simple Python project that translates English text into Hindi using Google Translate API.
+
+## Features
+- Translate English to Hindi
+- Simple command-line interface
+- Built using Python
+
+## Installation
+
+```bash
---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+def translate_text(text, src, dest):
+    url = f"https://translate.googleapis.com/translate_a/single?client=gtx&sl={src}&tl={dest}&dt=t&q={requests.utils.quote(text)}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        data = response.json()
+        return data[0][0][0]
+    else:
+        return "Translation failed"
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    translated_text = None
+    if request.method == 'POST':
+        text = request.form['text']
+        src = request.form['src']
+        dest = request.form['dest']
+        translated_text = translate_text(text, src, dest)
+    return render_template('index.html', translated_text=translated_text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
